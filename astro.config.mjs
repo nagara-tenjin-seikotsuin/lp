72504d5ddb3a826dc038befb45eee34bea48ff69
cochumo <stream99990@gmail.com>
2025-10-12 14:53:48 +0900

    Remove @astrojs/node adapter from Astro configuration and update package dependencies to streamline the project setup.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lp.nagara-tenjin-seikotsuin.com',
-  adapter: node({
-    mode: 'standalone'
-  }),
   vite: {
     plugins: [tailwindcss()],
   },

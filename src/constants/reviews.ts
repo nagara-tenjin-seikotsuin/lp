bb577208ed1db4408de6f5a14fc74e530fcafc17
cochumo <stream99990@gmail.com>
2025-10-12 16:17:56 +0900

    Refactor Reviews component to use static review data from constants, enhancing performance and simplifying API dependency.

---
+++ src/constants/reviews.ts
@@ -0,0 +1,125 @@
+export const REVIEWS = [
+  {
+    name: 'places/ChIJs9gziJirA2ARMDZVauQ4dVA/reviews/Ci9DQUlRQUNvZENodHljRjlvT2pWWFUwUTVhMjVqZVhORGJWWXRNak42TFRsblpFRRAB',
+    relativePublishTimeDescription: '3 か月前',
+    rating: 5,
+    text: {
+      text: '肩こりや足の甲に痛みがあり通い始めました。\n' +
+        '痛みはもちろん改善されていますが、原因のもとになるクセを改善できるように毎回軽いトレーニングもしてもらえます！\n' +
+        '毎回施術後は車のシートを直角にしないと気が済まないので、姿勢も改善されているのが分かりやすく感じられます！笑\n' +
+        '先生も明るく爽やかで毎回いろいろな話題でお話ししてくれるのでとても楽しく通わせていただいてます☺️',
+      languageCode: 'ja'
+    },
+    originalText: {
+      text: '肩こりや足の甲に痛みがあり通い始めました。\n' +
+        '痛みはもちろん改善されていますが、原因のもとになるクセを改善できるように毎回軽いトレーニングもしてもらえます！\n' +
+        '毎回施術後は車のシートを直角にしないと気が済まないので、姿勢も改善されているのが分かりやすく感じられます！笑\n' +
+        '先生も明るく爽やかで毎回いろいろな話題でお話ししてくれるのでとても楽しく通わせていただいてます☺️',
+      languageCode: 'ja'
+    },
+    authorAttribution: {
+      displayName: 'あちゃん',
+      uri: 'https://www.google.com/maps/contrib/115472671091122928596/reviews',
+      photoUri: 'https://lh3.googleusercontent.com/a/ACg8ocI1gmIdgyoQy2n_wxzaVpoNYhsHc0n9Q6XsP2TbDjgI8J_H6A=s128-c0x00000000-cc-rp-mo'
+    },
+    publishTime: '2025-07-12T02:40:52.170272483Z',
+    flagContentUri: 'https://www.google.com/local/review/rap/report?postId=Ci9DQUlRQUNvZENodHljRjlvT2pWWFUwUTVhMjVqZVhORGJWWXRNak42TFRsblpFRRAB&d=17924085&t=1',
+    googleMapsUri: 'https://www.google.com/maps/reviews/data=!4m6!14m5!1m4!2m3!1sCi9DQUlRQUNvZENodHljRjlvT2pWWFUwUTVhMjVqZVhORGJWWXRNak42TFRsblpFRRAB!2m1!1s0x6003ab988833d8b3:0x507538e46a553630'
+  },
+  {
+    name: 'places/ChIJs9gziJirA2ARMDZVauQ4dVA/reviews/ChZDSUhNMG9nS0VJQ0FnSUNQa2NxUFJBEAE',
+    relativePublishTimeDescription: '10 か月前',
+    rating: 5,
+    text: {
+      text: 'けがのケアと姿勢や運動のトレーニングでサッカーをしている息子がお世話になっています。1回の施術で痛みがなくなりました。姿勢のチェックやトレーニングもあり、自分の身体についてやトレーニングの意味や効果を知りながら取り組めます。先生が優しく丁寧で、少し遠いのですが、「ここに通いたい！」との息子希望で通わせていただいています。きれいで清潔な院内で、ハロウィンやクリスマスなど、季節も感じられます！',
+      languageCode: 'ja'
+    },
+    originalText: {
+      text: 'けがのケアと姿勢や運動のトレーニングでサッカーをしている息子がお世話になっています。1回の施術で痛みがなくなりました。姿勢のチェックやトレーニングもあり、自分の身体についてやトレーニングの意味や効果を知りながら取り組めます。先生が優しく丁寧で、少し遠いのですが、「ここに通いたい！」との息子希望で通わせていただいています。きれいで清潔な院内で、ハロウィンやクリスマスなど、季節も感じられます！',
+      languageCode: 'ja'
+    },
+    authorAttribution: {
+      displayName: '佐竹千絵',
+      uri: 'https://www.google.com/maps/contrib/116813479668524890483/reviews',
+      photoUri: 'https://lh3.googleusercontent.com/a/ACg8ocKpiyR-sYPoLyeEOcSrYTAs4wJGxSmqNoD48chiW2Xq11IGGw=s128-c0x00000000-cc-rp-mo'
+    },
+    publishTime: '2024-11-25T09:20:13.812961Z',
+    flagContentUri: 'https://www.google.com/local/review/rap/report?postId=ChZDSUhNMG9nS0VJQ0FnSUNQa2NxUFJBEAE&d=17924085&t=1',
+    googleMapsUri: 'https://www.google.com/maps/reviews/data=!4m6!14m5!1m4!2m3!1sChZDSUhNMG9nS0VJQ0FnSUNQa2NxUFJBEAE!2m1!1s0x6003ab988833d8b3:0x507538e46a553630'
+  },
+  {
+    name: 'places/ChIJs9gziJirA2ARMDZVauQ4dVA/reviews/ChdDSUhNMG9nS0VJQ0FnTUR3NDhHWTB3RRAB',
+    relativePublishTimeDescription: '6 か月前',
+    rating: 5,
+    text: {
+      text: '姿勢矯正のために来院しました。他の人が見ても自分でもわかるほど姿勢が良くなりました。姿勢矯正後も全身ほぐしてもらえて、いつも終わる時には体がすごく軽くなります。ストレッチや運動も取り入れるので、家でもできるものを教えてもらいながら、継続して体が軽く楽になります！先生とたわいもない話をして楽しみながら楽になってとても通って良かったです。',
+      languageCode: 'ja'
+    },
+    originalText: {
+      text: '姿勢矯正のために来院しました。他の人が見ても自分でもわかるほど姿勢が良くなりました。姿勢矯正後も全身ほぐしてもらえて、いつも終わる時には体がすごく軽くなります。ストレッチや運動も取り入れるので、家でもできるものを教えてもらいながら、継続して体が軽く楽になります！先生とたわいもない話をして楽しみながら楽になってとても通って良かったです。',
+      languageCode: 'ja'
+    },
+    authorAttribution: {
+      displayName: 'Yuka amnos',
+      uri: 'https://www.google.com/maps/contrib/106161458894983538568/reviews',
+      photoUri: 'https://lh3.googleusercontent.com/a-/ALV-UjX4A-sycThKyQdgzB1d8A-f458j6CPR-YNcspZth3fL1HxbSs0=s128-c0x00000000-cc-rp-mo'
+    },
+    publishTime: '2025-03-28T01:38:02.800298Z',
+    flagContentUri: 'https://www.google.com/local/review/rap/report?postId=ChdDSUhNMG9nS0VJQ0FnTUR3NDhHWTB3RRAB&d=17924085&t=1',
+    googleMapsUri: 'https://www.google.com/maps/reviews/data=!4m6!14m5!1m4!2m3!1sChdDSUhNMG9nS0VJQ0FnTUR3NDhHWTB3RRAB!2m1!1s0x6003ab988833d8b3:0x507538e46a553630'
+  },
+  {
+    name: 'places/ChIJs9gziJirA2ARMDZVauQ4dVA/reviews/ChdDSUhNMG9nS0VJQ0FnTUR3aGNXQ3VBRRAB',
+    relativePublishTimeDescription: '6 か月前',
+    rating: 5,
+    text: {
+      text: '猫背や巻肩を治すために通っていますが、先生も優しく接してくださり院内の雰囲気も明るくてとても心地が良いです。\n' +
+        '家でも真似してできるような運動を取り入れながら施術していただき猫背や巻肩も少しずつ改善できている気がします。\n' +
+        '以前は産後の骨盤矯正もお願いしていましたが、骨盤まわりもしっかりと締めていただいてウエストも細くなり腰の痛みも減りました。',
+      languageCode: 'ja'
+    },
+    originalText: {
+      text: '猫背や巻肩を治すために通っていますが、先生も優しく接してくださり院内の雰囲気も明るくてとても心地が良いです。\n' +
+        '家でも真似してできるような運動を取り入れながら施術していただき猫背や巻肩も少しずつ改善できている気がします。\n' +
+        '以前は産後の骨盤矯正もお願いしていましたが、骨盤まわりもしっかりと締めていただいてウエストも細くなり腰の痛みも減りました。',
+      languageCode: 'ja'
+    },
+    authorAttribution: {
+      displayName: '氏家茉由',
+      uri: 'https://www.google.com/maps/contrib/106027566916363184159/reviews',
+      photoUri: 'https://lh3.googleusercontent.com/a-/ALV-UjW8KsbWvcuIwteoYQmmAS7dqgy3RX7IUJomvUEE-2maAKml2agI=s128-c0x00000000-cc-rp-mo'
+    },
+    publishTime: '2025-03-27T02:55:03.020026Z',
+    flagContentUri: 'https://www.google.com/local/review/rap/report?postId=ChdDSUhNMG9nS0VJQ0FnTUR3aGNXQ3VBRRAB&d=17924085&t=1',
+    googleMapsUri: 'https://www.google.com/maps/reviews/data=!4m6!14m5!1m4!2m3!1sChdDSUhNMG9nS0VJQ0FnTUR3aGNXQ3VBRRAB!2m1!1s0x6003ab988833d8b3:0x507538e46a553630'
+  },
+  {
+    name: 'places/ChIJs9gziJirA2ARMDZVauQ4dVA/reviews/ChZDSUhNMG9nS0VJQ0FnTUNJd1BHcFZnEAE',
+    relativePublishTimeDescription: '6 か月前',
+    rating: 5,
+    text: {
+      text: '肩凝りや頭痛に悩んでいたところ、妻に紹介してもらい伺いました。\n' +
+        '初診では歩き方や身体の傾きなどを観察していただき、ボードを使った説明を聞いて症状の原因を知ることができました。\n' +
+        '週1回の通院では様々なトレーニングを行い、また、自宅で出来るストレッチメニューを教えてもらっています。\n' +
+        '２ヶ月程継続していますが、頭痛の症状が出る頻度が激減するなど身体の変化を感じています。\n' +
+        'これからも整骨院でのトレーニングや自宅でのストレッチを継続して、しっかり自分の身体と向き合い続けていきます！！',
+      languageCode: 'ja'
+    },
+    originalText: {
+      text: '肩凝りや頭痛に悩んでいたところ、妻に紹介してもらい伺いました。\n' +
+        '初診では歩き方や身体の傾きなどを観察していただき、ボードを使った説明を聞いて症状の原因を知ることができました。\n' +
+        '週1回の通院では様々なトレーニングを行い、また、自宅で出来るストレッチメニューを教えてもらっています。\n' +
+        '２ヶ月程継続していますが、頭痛の症状が出る頻度が激減するなど身体の変化を感じています。\n' +
+        'これからも整骨院でのトレーニングや自宅でのストレッチを継続して、しっかり自分の身体と向き合い続けていきます！！',
+      languageCode: 'ja'
+    },
+    authorAttribution: {
+      displayName: 't s',
+      uri: 'https://www.google.com/maps/contrib/115989087397834606368/reviews',
+      photoUri: 'https://lh3.googleusercontent.com/a-/ALV-UjUrWXptUS4mrs3iTkUaDh82j8GYKpDdhlsYkjVqWhoUyNRA4ZXL=s128-c0x00000000-cc-rp-mo'
+    },
+    publishTime: '2025-03-29T12:02:16.825706Z',
+    flagContentUri: 'https://www.google.com/local/review/rap/report?postId=ChZDSUhNMG9nS0VJQ0FnTUNJd1BHcFZnEAE&d=17924085&t=1',
+    googleMapsUri: 'https://www.google.com/maps/reviews/data=!4m6!14m5!1m4!2m3!1sChZDSUhNMG9nS0VJQ0FnTUNJd1BHcFZnEAE!2m1!1s0x6003ab988833d8b3:0x507538e46a553630'
+  }
+]
